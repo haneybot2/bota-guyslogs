78c61c4b7992818beecb0e28f98b26d2eff54f68
haneybot2 <41707988+haneybot2@users.noreply.github.com>
2018-09-10 21:36:14 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -66,21 +66,21 @@ let args = message.content.split(' ').slice(1).join(' ');
             
             if (sigMessage === "online") {
                 client.user.setStatus("online");
-                message.author.send("Your status was set to online.");
+                message.channel.send("Your status was set to online.");
             }
             if (sigMessage === "idle") {
                 client.user.setStatus("idle");
-                message.author.send("Your status was set to idle.");
+                message.channel.send("Your status was set to idle.");
             }
             if (sigMessage === "invisible") {
                 client.user.setStatus("invisible");
-                message.author.send("Your status was set to invisible.");
+                message.channel.send("Your status was set to invisible.");
             }
             if (sigMessage === "dnd") {
                 client.user.setStatus("dnd");
-                message.author.send("Your status was set to dnd.");
+                message.channel.send("Your status was set to dnd.");
             }
-            // message.author.send("." + message.content);
+           
         
 }
 });
